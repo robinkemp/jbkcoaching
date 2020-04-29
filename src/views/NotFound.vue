84bd953ec3b261343b4c5784dd65940eedd8eb36
RobK <robinkemp@gmail.com>
2020-04-29 23:48:30 +0100

    typo

--- src/views/NotFound.vue
+++ src/views/NotFound.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="about">
     <h1>Back it up</h1>
-    <router-link to="/hoe">Time to go home</router-link>
+    <router-link to="/">Time to go home</router-link>
   </div>
 </template>
